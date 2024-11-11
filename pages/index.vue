d4a868ea34709a59e53f934e36898b5bf86ce219
wahyugautama <wayanwahyugautama@gmail.com>
2024-11-11 23:47:51 +0800

    change label

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="h-screen w-full flex items-center justify-center">
-        <a class="hover:text-gray-500" target="_blank" href="https://www.sandikalastudio.com">sandikalastudio.com</a>
+        <a class="hover:text-gray-500" target="_blank" href="https://www.sandikalastudio.com">my-website-studio</a>
     </div>
 </template>
