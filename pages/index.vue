2691d89028da9f726ddebb4ce9b99c0691b21f4c
wahyugautama <wayanwahyugautama@gmail.com>
2024-11-11 23:45:11 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-screen w-full flex items-center justify-center">
+        <a class="hover:text-gray-500" target="_blank" href="https://www.sandikalastudio.com">sandikalastudio.com</a>
+    </div>
+</template>
