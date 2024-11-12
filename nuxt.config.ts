8e09ee389b2e514beef7f743bfbd6ac75e866bb1
wahyugautama <wayanwahyugautama@gmail.com>
2024-11-12 22:59:08 +0800

    remove tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  css: ["@/assets/css/reset.css", "@/assets/css/main.css"],
   devtools: { enabled: true },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
-  modules: ["@nuxtjs/tailwindcss"],
 });
